eab7f51d8df0d1431be3e10a414fd5a238dff0ee
Michal Kunert <mkunert@virtuslab.com>
2024-02-27 21:19:44 +0100

    using fetch and axios to log request duration

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,37 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  return config;
+// // Add a request interceptor
+// axios.interceptors.request.use(function (config) {
+//   config.startTime = new Date();
+//   return config;
+// });
+
+// // Add a response interceptor
+// axios.interceptors.response.use(function (response) {
+//   const endTime = new Date();
+//   const duration = endTime - response.config.startTime;
+//   console.log(`Zapytanie trwało ${duration}ms`);
+//   return response;
+// });
+
+// const {
+//   data: { articles },
+// } = await axios.get('/api/data/articles?timeout=3000');
+
+// document.querySelector('#data').innerHTML = articles[0].content;
+
+const fetchProxy = new Proxy(fetch, {
+  apply(target, _, argumentsList) {
+    const now = new Date();
+    return target(...argumentsList).then((response) => {
+      const duration = new Date() - now;
+      console.log(`Zapytanie trwało ${duration}ms`);
+      return response;
+    });
+  },
 });
 
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Do something with response data
-  return response;
-});
+const response = await fetchProxy('/api/data/articles?timeout=2000');
+const data = await response.json();
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
-
-document.querySelector('#data').innerHTML = articles[0].content;
+document.querySelector('#data').innerHTML = data.articles[0].content;
