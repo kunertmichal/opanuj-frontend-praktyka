8f40ec55463e8c5c28daf82d771494e528f1e656
Michal Kunert <mkunert@virtuslab.com>
2024-03-01 16:40:05 +0100

    add svelte app

---
+++ practice/svelte-app/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "./app.css";
+  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+
+  const queryClient = new QueryClient()
+</script>
+
+<div>
+  <QueryClientProvider client={queryClient}>
+    <div>App body</div>
+  </QueryClientProvider>
+</div>
